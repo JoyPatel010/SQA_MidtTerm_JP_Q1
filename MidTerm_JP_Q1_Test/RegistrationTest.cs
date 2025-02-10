8dc2eb5451b9efd62feef844e854a70c18043ef3
Joy Patel <pateljoy149@gmail.com>
2025-02-10 14:15:58 -0500

    THree test cases added

--- MidTerm_JP_Q1_Test/RegistrationTest.cs
+++ MidTerm_JP_Q1_Test/RegistrationTest.cs
@@ -5,11 +5,68 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using MidTerm_JP_Q1;
+using System.Runtime.Remoting.Messaging;
+using NUnit.Framework.Constraints;
 
 namespace MidTerm_JP_Q1_Test
 {
+    [TestFixture]
     public class RegistrationTest
     {
+        
+
+        [Test]
+        public void ValidName_InputJoy_OutputValidName()
+        {
+            Registration registration=new Registration();
+            string validName = "Joy";
+
+            string actual = registration.RegisterName(validName);
+
+            Assert.That(validName, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void InvalidName_InputEmptyStringName_OutputError()
+        {
+            Registration registration = new Registration();
+            string validName = "";
+
+            var actual = Assert.Throws<ArgumentException>(() => registration.RegisterName(validName));
+
+            Assert.That(validName, Is.EqualTo(actual.ToString()));
+        }
+
+        [Test]
+        public void ValidAge_Input45_OutputValidAge()
+        {
+            Registration registration = new Registration();
+            int validAge = 45;
+
+            int actual = registration.RegisterAge(validAge);
+
+            Assert.That(validAge, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void ValidGroupSize_Input15_OutputValidGroupSize()
+        {
+            Registration registration = new Registration();
+            int validGroupSize = 15;
+
+            int actual = registration.RegisterGroupSize(validGroupSize);
+
+            Assert.That(validGroupSize, Is.EqualTo(actual));
+        }
+
+
+
+
+
+
+
+
+
 
 
     }
