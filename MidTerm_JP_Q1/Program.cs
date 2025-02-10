5fc5a3adaa7b511378b81c5815b8abf70c1db703
Joy Patel <pateljoy149@gmail.com>
2025-02-10 13:16:08 -0500

    Inititalized and Added Empty Project

---
+++ MidTerm_JP_Q1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm_JP_Q1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ MidTerm_JP_Q1_Test/RegistrationTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MidTerm_JP_Q1;
+
+namespace MidTerm_JP_Q1_Test
+{
+    public class RegistrationTest
+    {
+
+
+    }
+}
